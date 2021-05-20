dc09084f872a37471b0cc3f38ba490d0fab1f508
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-05-20 10:44:22 +0200

    Funktion 02a + 02b ++

--- script_01.js
+++ script_01.js
@@ -10,5 +10,34 @@ console.log("Hallo Jan, das ist ein test");
 }
 
 // Funktionsaufruf (call)
-test();
+//test();
 
+
+/***** Funktionen 02a *****/
+// Parametrisierung + Datenübergabe von Innen
+
+function ausgabeName ()
+{
+    let firstName = "Deniz"; // Variable ist lokal! --> Nur innerhalb der funktion gültig
+    console.log("Hallo " + firstName + "!");
+}
+
+// ausgabeName();
+// console.log(firstName);
+
+
+/***** Funktionen 02b *****/
+// Parametrisierung + Datenübergabe von Außen
+
+function ausgabeNameParam (firstName) //Parameter, wird hier definiert
+{
+    // wenn firstName leer, dann "nobody"
+    if (firstName == undefined) {
+        firstName = "nobody";
+    }
+    console.log("Hallo " + firstName + "!");    
+}
+
+ausgabeNameParam("Deniz"); // Call + Argument
+ausgabeNameParam(); // Call ohne Argument
+//ausgabeNameParam(prompt("Bitte Namen eingeben")); // Call mit Abfrage
