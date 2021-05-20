aa3b0ce487d24b6586613d66fd6f46e3fd455a7a
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-05-20 09:09:32 +0200

    Kapselung von Codeblöcken

--- script_01.js
+++ script_01.js
@@ -2,3 +2,13 @@
 /***** Funktionen 01 *****/
 
 // 1. Kapselung von Codeblöcken
+
+// Funktionsrumpf (body)
+function test()
+{
+console.log("Hallo Jan, das ist ein test");
+}
+
+// Funktionsaufruf (call)
+test();
+
